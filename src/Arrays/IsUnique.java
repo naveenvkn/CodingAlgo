6fdd7da9ee88a3645de4a1d5170c23566432f86c
naveenvkn <naveenvkn@gmail.com>
2017-11-07 01:00:13 -0500

    Initial commit

---
+++ src/Arrays/IsUnique.java
@@ -0,0 +1,32 @@
+// Program to check if the given set of words has unique characters in each word.
+package Arrays;
+
+public class IsUnique {
+	
+	public static boolean checkUniqueChars(String str)
+	{
+		if(str.length() > 128)
+			return false;
+		boolean[] ind=new boolean[128];
+		for(int i=0;i<str.length();i++)
+		{
+			int val=str.charAt(i);
+			if(ind[val])
+				return false;
+			ind[val]=true;
+		}
+		return true;
+		
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String words[]= {"Hello","Naveen","How","are","you"};
+		for ( String word : words)
+		{
+			System.out.println("Is "+word+"has unique chars: "+checkUniqueChars(word));
+		}
+
+	}
+
+}
